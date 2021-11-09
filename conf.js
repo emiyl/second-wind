4962453938bc5ea15b669420505c38985a05b43b
emiyl <me@emiyl.com>
2021-11-09 21:10:25 +0000

    updated conf

--- conf.js
+++ conf.js
@@ -2,23 +2,23 @@ const title = "Second Wind";
 
 module.exports = {
 	locales: {
-    title: title,
-    description: 'A large-scale expansion mod for Breath of the Wild.'
+		title: title,
+		description: 'A large-scale expansion mod for Breath of the Wild.'
 	},
 
 	search: 'Search',
-	
-	themeConfigLocales: {
-  	selectLanguageName: title,
 
-  	backToHome: "Take me home",
-  	contributorsText: "Contributors",
-  	editLinkText: "Edit this page",
-  	lastUpdatedText: "Last Updated",
-  	openInNewWindow: "Open in new window",
-  	selectLanguageAriaLabel: "Select language",
-  	toggleDarkMode: "Toggle dark mode",
-  	toggleSidebar: "Toggle sidebar",
-  	discordNoticeText: "If you need further help, ask on the Zelda Modding [Discord Server](https://discord.gg/jRMRv58) for assistance.",
+	themeConfigLocales: {
+		discordNoticeText: "If you need further help, ask on the Zelda Modding [Discord Server](https://discord.gg/jRMRv58) for assistance.",
+
+		selectLanguageName: title,
+		backToHome: "Take me home",
+		contributorsText: "Contributors",
+		editLinkText: "Edit this page",
+		lastUpdatedText: "Last Updated",
+		openInNewWindow: "Open in new window",
+		selectLanguageAriaLabel: "Select language",
+		toggleDarkMode: "Toggle dark mode",
+		toggleSidebar: "Toggle sidebar",
 	},
 };
