a5b2e0dd05bfeb847cf65a47fd1ae453cb066a8a
emiyl <me@emiyl.com>
2021-11-09 19:48:43 +0000

    initial

---
+++ conf.js
@@ -0,0 +1,24 @@
+const title = "Second Wind";
+
+module.exports = {
+	locales: {
+    title: title,
+    description: 'A large-scale expansion mod for Breath of the Wild.'
+	},
+
+	search: 'Search',
+	
+	themeConfigLocales: {
+  	selectLanguageName: title,
+
+  	backToHome: "Take me home",
+  	contributorsText: "Contributors",
+  	editLinkText: "Edit this page",
+  	lastUpdatedText: "Last Updated",
+  	openInNewWindow: "Open in new window",
+  	selectLanguageAriaLabel: "Select language",
+  	toggleDarkMode: "Toggle dark mode",
+  	toggleSidebar: "Toggle sidebar",
+  	discordNoticeText: "If you need further help, ask on the Zelda Modding [Discord Server](https://discord.gg/jRMRv58) for assistance.",
+	},
+};
